e2192b1937f92ddc98a7cbeffb69d99b9034d759
Kagetora <jessicanovia59@gmail.com>
2022-01-16 21:02:10 +0700

    navbar login and logout setup

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="collapse navbar-collapse d-flex justify-content-beetween" id="nav-bar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-brand" href="index.php">FEHU</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="navlink" href="login.php">Login</a>
+            </li>
+        </ul>
+        
+        </ul>
+    </div>
