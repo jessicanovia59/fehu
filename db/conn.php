34e8ec6ad32c1db14270882948e9a6ed5807c682
Kagetora <jessicanovia59@gmail.com>
2022-01-16 21:00:35 +0700

    conn.php update

--- db/conn.php
+++ db/conn.php
@@ -2,6 +2,6 @@
     $host = 'localhost';
     $username = 'root';
     $password = '';
-    $database = 'fehu';
+    $database = 'gogame';
     $conn = mysqli_connect($host, $username, $password, $database);
 ?>
